64f42fc79307b7f91a169d670963fd38027c2f12
Tyler Long <operator27c@protonmail.com>
2022-01-07 12:34:22 -0500

    Update digitalocean-dns-sync.go

--- go/digitalocean-dns-sync.go
+++ go/digitalocean-dns-sync.go
@@ -34,7 +34,7 @@ func main() {
     var my_domain = ""
     var my_domain_id=285678041
     
-    if len(os.Args) >= 4 { 
+    if len(os.Args) >= 3 { 
       my_host = os.Args[1]
       my_domain = os.Args[2]
     } else {
